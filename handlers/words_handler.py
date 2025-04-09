a2f0dee924471ac1c09e311a0c452ac3e7cceb76
EliboyevElbek <coderpython107@gmail.com>
2025-04-09 22:35:17 +0500

    8-version

--- handlers/words_handler.py
+++ handlers/words_handler.py
@@ -52,7 +52,7 @@ async def view_words_handler(callback: CallbackQuery, state: FSMContext):
         info = f'<b>{cat_name.upper()}</b> toifasiga tegishli so\'zlar <b>(1-{len(words[:10])})</b>\n\n'
         count = 1
         for word in words[:10]:
-            info += f"<b>{count}. {word[1]} ——— {word[0]}</b>\n"
+            info += f"<b>{count}. {word[1].lower()} ——— {word[0].lower()}</b>\n"
             count += 1
         await callback.message.answer(
             text=info,
@@ -82,7 +82,7 @@ async def kb_handler(call: CallbackQuery, state: FSMContext):
         info = f'<b>{cat_name.upper()}</b> toifasiga tegishli so\'zlar <b>({l*10 + 1}-{l*10 + len(words10[l])})</b>\n\n'
         count = 1
         for word in words10[l]:
-            info += f"<b>{count}. {word[ss]} ——— {word[0]}</b>\n"
+            info += f"<b>{count}. {word[ss].lower()} ——— {word[0].lower()}</b>\n"
             count += 1
         await call.message.edit_text(
             text=info,
@@ -96,7 +96,7 @@ async def kb_handler(call: CallbackQuery, state: FSMContext):
         info = f'<b>{cat_name.upper()}</b> toifasiga tegishli so\'zlar<b>({l*10 + 1}-{l*10 + len(words10[l])})</b>\n\n'
         count = 1
         for word in words10[l]:
-            info += f"<b>{count}. {word[ss]} ——— {word[0]}</b>\n"
+            info += f"<b>{count}. {word[ss].lower()} ——— {word[0].lower()}</b>\n"
             count += 1
         await call.message.edit_text(
             text=info,
@@ -107,7 +107,7 @@ async def kb_handler(call: CallbackQuery, state: FSMContext):
         info = f'<b>{cat_name.upper()}</b> toifasiga tegishli so\'zlar<b>({l * 10 + 1}-{l * 10 + len(words10[l])})</b>\n\n'
         count = 1
         for word in words10[l]:
-            info += f"<b>{count}. {word[2]} ——— {word[0]}</b>\n"
+            info += f"<b>{count}. {word[2].lower()} ——— {word[0].lower()}</b>\n"
             count += 1
         await call.message.edit_text(
             text=info,
@@ -118,7 +118,7 @@ async def kb_handler(call: CallbackQuery, state: FSMContext):
         info = f'<b>{cat_name.upper()}</b> toifasiga tegishli so\'zlar<b>({l * 10 + 1}-{l * 10 + len(words10[l])})</b>\n\n'
         count = 1
         for word in words10[l]:
-            info += f"<b>{count}. {word[1]} ——— {word[0]}</b>\n"
+            info += f"<b>{count}. {word[1].lower()} ——— {word[0].lower()}</b>\n"
             count += 1
         await call.message.edit_text(
             text=info,
